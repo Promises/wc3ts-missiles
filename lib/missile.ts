f8d20b03aa496695e634c44b7fbf673aebcda821
Henning Berge <henning.ber@gmail.com>
2020-07-12 14:13:10 +0200

    finish basic missile library

--- lib/missile.ts
+++ lib/missile.ts
@@ -1,232 +1,197 @@
-import { Players } from "w3ts/globals";
-import { Unit, Item, MapPlayer, Timer, Handle, Point, Trigger, Effect, Rectangle, Group } from "w3ts";
+import {MapPlayer, Timer, Unit, Trigger, Effect, Group} from "w3ts";
 
 
-
-const timerTimeout: number = 0.03125;
-const playerNeutralPassive: MapPlayer = MapPlayer.fromIndex(PLAYER_NEUTRAL_PASSIVE);
-const missileTimers: Map<Handle, Timer> = new Map(); //Map of our timers indexed by the Handle
-const missiles: Map<Handle, Missile> = new Map(); //Map of our missiles
-let fixedRotationAxes: boolean = false;
-
-
-enum CollisionType {
-    CIRCLE,
-    RECTANGLE
+export interface MissileOptions {
+    distance?: number;
+    duration?: number;
+    hitRadius?: number;
+    collides?: boolean;
+    heroOnly?: number;
+    allies?: boolean;
+    excludeUnit?: Unit;
+    dmgFunc?: () => void;
+    effectModel?: string;
+    effectDmgModel?: string;
+    height?: number;
+    scale?: number;
+    timerFunc?: () => void;
+    attackType?: attacktype;
+    damageType?: damagetype;
 }
 
-abstract class Missile {
-    private static readonly TIMER_TIMEOUT: number = timerTimeout;
-    private static readonly NEUTRAL_PASSIVE: MapPlayer = playerNeutralPassive;
-    private static readonly MAXIMUM_COLLISION_SIZE: number = 197.0;
-    private static readonly COLLISION_ACCURACY_FACTOR: number = 1.0;
-    private static readonly USE_COLLISION_Z_FILTER: boolean = true;
-    private static readonly USE_DESTRUCTABLE_FILTER: boolean = true;
-    private static readonly USE_item_FILTER: boolean = true;
+export class Missile {
+    static MissileTimer = new Timer()    // global missile Timer.
+    static MissileCadence = .03           // how fast the missile updates and checks for collision (higher = less accurate).
+    static MissileStack: Missile[] = []            // where instances of missiles are stored.
+    static Missile = {}            // initialize class table.
+    private ownerUnit: Unit;
+    private owner: MapPlayer;
+    private landingX: number;
+    private landingY: number;
+    private missileX: number;
+    private missileY: number;
+    private missileZ: number;
+    private traveled: number;
+    private velocity: number;
+    private angle: number;
+    private effect: Effect;
 
 
-    // GLOBALS l 472
-    private CORE: Trigger = new Trigger();
-    private MOVE: Trigger = new Trigger();
-    private TMR: Timer = new Timer();
-    private LOC: location = Location(0, 0);
-    private RECT: Rectangle = new Rectangle(0, 0, 0, 0);
-    private GROUP: Group = new Group();
-    // For starting and stopping the timer.
-    private active: number = 0
-    // Arrays for data structure.
-    private instances: number[];
-    private missileList: Missile[];
-    private expression: (() => boolean)[];
-    private condition: boolexpr;
-    private removed: number[];
-    private destroying: boolean[];
-    private recycling: boolean[];
-    // Internal widget filter functions.
-    private destFilter: boolexpr;
-    private itemFilter: boolexpr;
-    private UnitFilter: boolexpr;
-    private table: ;
+    private effectModel: string = 'Abilities\\Weapons\\FireBallMissile\\FireBallMissile.mdl'
+    private effectDmgModel: string = 'Abilities\\Weapons\\FireBallMissile\\FireBallMissile.mdl'
+    private height: number = 60.0
+    private scale: number = 1.0
+    private duration: number = 1.5
+    private distance: number = 600.0
+    private damage: number = 100.0
+    private hitRadius: number = 65.0
+    private collides: boolean = false
+    private heroOnly: boolean = false
+    private allies: boolean = false
+    private dmgFunc: (() => void) | undefined = undefined
+    private timerFunc: (() => void) | undefined = undefined
+    private excludeUnit: Unit | undefined = undefined
+    private aType: attacktype = ATTACK_TYPE_NORMAL
+    private dType: damagetype = DAMAGE_TYPE_NORMAL
+
+    private searchGroup: Group = new Group();
+    private damageGroup: Group = new Group();
+    private filter: (u: Unit) => boolean;
 
 
-    source: Unit
-    target: Unit
-    distance: number
-    owner: player
-    speed: number
-    acceleration: number
-    damage: number
-    turn: number
-    data: number
-    recycle: boolean
-    wantDestroy: boolean
-    collision: number
-    collisionZ: number
+    constructor(ownerUnit: Unit, landingX: number, landingY: number, options?: MissileOptions) {
+        this.ownerUnit = ownerUnit;
+        this.owner = ownerUnit.owner;
+        this.landingX = landingX;
+        this.landingY = landingY;
 
-    readonly allocated: boolean
-    readonly id: number
-    readonly dummy: Unit
-    readonly origin: MissilePosition
-    readonly impact: MissilePosition
-    readonly terrainZ: number
-    readonly x: number
-    readonly y: number
-    readonly z: number
-    readonly angle: number
+        this.missileX = ownerUnit.x
+        this.missileY = ownerUnit.y
+        this.missileZ = 0.0
+        this.traveled = 0.0
 
-
-    private static GetUnitBodySize(u: Unit): number {
-        return 100.0;
-    }
-
-    private static GetDestructableHeight(d: destructable): number {
-        return GetDestructableOccluderHeight(d);
-    }
-
-    private static GetitemHeight(i: Item): number {
-        return 16.0;
-    }
-
-    private static ToggleUnitIndex(enable: boolean): boolean {
-        return true;
-    }
-
-    public GetLocZ(x: number, y: number) {
-        MoveLocation(this.LOC, x, y)
-        return GetLocationZ(this.LOC)
-    }
-
-
-}
-
-class EulerAngles {
-    private static XY_STEPS: number = 420
-    private static Z_STEPS: number = 270
-    private static angleTable: Map<number, number>[];
-    private static xyDelta: number = (2.00 * Math.PI) / EulerAngles.XY_STEPS
-    private static zDelta: number = (2.00 * Math.PI) / EulerAngles.Z_STEPS
-    private static zSteps: number = 0
-    static yaw: number = 0.00
-    static pitch: number = 0.00
-    static roll: number = 0.00
-
-    static from(xyAngle: number, zAngle: number): void {
-        xyAngle = xyAngle % 2.00 * Math.PI;
-        zAngle = zAngle % 2.00 * Math.PI;
-        let key = Math.floor(zAngle/this.zDelta) *this.XY_STEPS + Math.floor(xyAngle/this.xyDelta);
-        EulerAngles.yaw = this.angleTable[0].get(key);
-        EulerAngles.pitch = this.angleTable[1].get(key);
-        EulerAngles.roll = this.angleTable[2].get(key);
-    }
-
-    static initAngles(key: number, dx: number, dy: number, dz: number): void {
-        let N: number = dx*dx + dy*dy;
-        let norm: number = N + dz*dz;
-        
-        if (norm === 0 ) {
-            return;
-        }
-        norm = 1.00001 * Math.sqrt(norm);
-        dx = dx/norm;
-        dy = dy/norm;
-        dz = dz/norm;
-        if(N === 0 ) {
-            this.angleTable[0].set(key, 0.00);
-            this.angleTable[2].set(key, 0.00);
-
-            if (dz > 0) {
-                this.angleTable[1].set(key, -Math.PI*0.5)
-            } else {
-                this. angleTable[1].set(key, Math.PI*0.5)
+        for (const key in options) {
+            if (options.hasOwnProperty(key) && this.hasOwnProperty(key)) {
+                this[key] = options[key];
             }
-
-            return
         }
-        N = Math.sqrt(N);
-        let pitch;
-        let cp;
-        if( dy < 0 ) {
-            pitch = Math.asin(dx*dz/N) + Math.PI;
-            cp = Math.cos(pitch);
-            if(fixedRotationAxes) {
-                this.angleTable[0].set(key,Math.acos(dx/cp));
-                this.angleTable[2].set(key,-Math.asin(dy*dz/(N*cp)) + Math.PI);
-            } else {
-                this.angleTable[2].set(key, Math.acos(dx/cp));
-                this.angleTable[0].set(key, -Math.asin(dy*dz/(N*cp)) + Math.PI);
-            }
+
+        if (this.allies) {
+            this.filter = (u: Unit) => this.filterAllies(u);
         } else {
-            pitch = -Math.asin(dx*dz/N);
-            cp = Math.cos(pitch);
-            if(fixedRotationAxes) {
-                this.angleTable[0].set(key, Math.acos(dx/cp));
-                this.angleTable[2].set(key, -Math.asin(dy*dz/(N*cp)));
+            this.filter = (u: Unit) => this.filterEnemies(u);
+
+        }
+
+        this.velocity = this.distance / (this.duration / Missile.MissileCadence);
+        this.angle = AngleBetweenPointsXY(this.missileX, this.missileY, landingX, landingY);
+        if (!this.effect) {
+            this.effect = new Effect(this.effectModel, this.missileX, this.missileY);
+        }
+    }
+
+    private addToStack(): void {
+        Missile.MissileStack.push(this);
+        this.startTimer();
+    }
+
+    private startTimer(): void {
+        if (Missile.MissileStack.length === 1 && Missile.MissileTimer.remaining === 0.0) {
+            Missile.MissileTimer.start(Missile.MissileCadence, true, Missile.updateStack);
+        }
+    }
+
+    static timerCheck(): void {
+        if (Missile.MissileStack.length < 1) {
+            Missile.MissileTimer.pause();
+        }
+    }
+
+    static updateStack(): void {
+        for (let missile of this.MissileStack) {
+            if (missile.traveled > missile.distance) {
+                missile.destroy()
             } else {
-                this.angleTable[2].set(key, Math.acos(dx/cp));
-                this.angleTable[0].set(key, -Math.asin(dy*dz/(N*cp)));
+                missile.update();
             }
         }
-        this.angleTable[0].set(key, pitch);
     }
 
+    private destroy(): void {
+        this.effect.destroy();
+        this.damageGroup.destroy();
+        this.searchGroup.destroy();
+        Missile.MissileStack = Missile.MissileStack.filter(value => value !== this);
+        Missile.timerCheck();
+    }
 
-    static initOrientationTables(zStep: number): void {
-        let xy = Math.cos(zStep*EulerAngles.zDelta);
-        let z = Math.sin(zStep*EulerAngles.zDelta);
-        let xySteps = 0;
-        while (xySteps !== this.XY_STEPS) {
-            this.initAngles(zStep*this.XY_STEPS + xySteps, xy*Math.cos(xySteps*this.xyDelta), xy*Math.sin(xySteps*this.xyDelta), z)
-            xySteps++;
+    private update(): void {
+        this.traveled = this.traveled + this.velocity;
+        [this.missileX, this.missileY] = PolarProjectionXY(this.missileX, this.missileY, this.velocity, this.angle);
+        this.missileZ = GetTerrainCliffLevel(this.missileX, this.missileY) * 128 - 256 + this.height;
+        this.effect.setYaw(this.angle * bj_DEGTORAD);
+        this.effect.x = this.missileX;
+        this.effect.y = this.missileY;
+        this.effect.z = this.missileZ;
+        if (this.timerFunc) {
+            this.timerFunc();
+        }
+        this.collisionCheck();
+    }
+
+    private collisionCheck() {
+        this.searchGroup.enumUnitsInRange(this.missileX, this.missileY, this.hitRadius, Condition(() => {
+            let unit: Unit = Group.getFilterUnit();
+            return unit && unit === this.excludeUnit;
+        }))
+        if (this.searchGroup.size > 0) {
+            GroupUtilsAction(this.searchGroup, (unit: Unit) => {
+                this.damageGroup.addUnit(unit);
+                this.searchGroup.removeUnit(unit);
+            })
         }
     }
 
-    static initTables(): void {
-        this.initOrientationTables(this.zSteps);
-        this.initOrientationTables(this.zSteps + this.Z_STEPS/2);
+    private filterAllies(u: Unit) {
+        return u.isAlly(this.owner)
+            && u.isAlive()
+            && !u.isUnitType(UNIT_TYPE_STRUCTURE)
+            && !u.inGroup(this.damageGroup);
     }
 
-    static onScopeInit() {
-        this.angleTable = [];
-        for (let i = 0; i < 3; i++) {
-            this.initTables[i] = new Map<number,number>();
+    private filterEnemies(u: Unit) {
+        return u.isEnemy(this.owner)
+            && u.isAlive()
+            && !u.isUnitType(UNIT_TYPE_STRUCTURE)
+            && !u.isUnitType(UNIT_TYPE_MAGIC_IMMUNE)
+            && !u.inGroup(this.damageGroup);
+    }
+}
+
+
+// :: get facing angle from A to B
+// @x1,y1 = point A (facing from)
+// @x2,y2 = point B (facing towards)
+function AngleBetweenPointsXY(x1, y1, x2, y2): number {
+    return bj_RADTODEG * Atan2(y2 - y1, x2 - x1)
+}
+
+
+// :: PolarProjectionBJ converted to x,y
+// @x1,y1 = origin coord
+// @d = distance between origin and direction
+// @a = angle to project
+function PolarProjectionXY(x1, y1, d, a): [number, number] {
+    let x = x1 + d * Cos(a * bj_DEGTORAD)
+    let y = y1 + d * Sin(a * bj_DEGTORAD)
+    return [x, y]
+}
+
+function GroupUtilsAction(group: Group, callback: (unit: Unit) => void): void {
+    let size = group.size;
+    if (size > 0) {
+        for (let i = 0; i < size; i++) {
+            callback(group.getUnitAt(i));
         }
-        fixedRotationAxes = true;
-        while (this.zSteps !== this.Z_STEPS/2) {
-            this.initTables();
-            this.zSteps++;
-        }
     }
 }
-
-//Node from Missile.j
-class MissileStack {
-    static stack: Missile[] = [];
-    
-    public push(item: Missile) {
-        MissileStack.stack.push(item);
-    }
-
-    public pop(): Missile {
-        MissileStack.stack.reverse;
-        let item = MissileStack.stack.pop();
-        MissileStack.stack.reverse;
-        return item;
-    }
-}
-
-class MissileEffects {
-    effectHandle: Handle;
-    missile: Missile;
-    //lookupTable[0].get(key)
-    //lookupTable[0].set(key, value)
-    //lookupTable[0].has(key)
-    
-    handle(): EffectHandle {
-        return ;
-    }
-
-    getMissile(): Missile {
-        return this.missile;
-    }
-}
-
